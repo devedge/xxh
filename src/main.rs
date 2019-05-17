a327905c9d6f18c2c6f7e23762bfb88302cfcca9
devedge <richard.pjski2@gmail.com>
2019-05-16 21:08:52 -0400

    Example code taken from the twox-hash crate as a basis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::hash::Hasher;
+use std::io::{BufRead, BufReader};
+use twox_hash::XxHash;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in env::args().skip(1) {
+        let f = File::open(&arg).unwrap();
+        let mut f = BufReader::new(f);
+
+        let mut hasher = XxHash::with_seed(0);
+
+        loop {
+            let consumed = {
+                let bytes = f.fill_buf().unwrap();
+                if bytes.len() == 0 {
+                    break;
+                }
+                hasher.write(bytes);
+                bytes.len()
+            };
+            f.consume(consumed);
+        }
+
+        println!("{:16x}   {}", hasher.finish(), arg);
+    }
 }
