3c521ffe29bc964c9612fe3f0cc86cd3ab3d103b
devedge <richard.pjski2@gmail.com>
2019-06-04 22:17:37 -0400

    Linting and fixed cursor position for multi-file hash output

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-// use std::env;
+use crossbeam_channel::bounded;
+use crossterm::{cursor, terminal, ClearType};
+use number_prefix::{NumberPrefix, Prefixed, Standalone};
 use std::fs::File;
 use std::hash::Hasher;
 use std::io::{BufRead, BufReader};
-use twox_hash::XxHash;
-
+use std::path::PathBuf;
 use std::thread;
 use std::time::{Duration, SystemTime};
-
-use crossterm::{ClearType, cursor, terminal};
-
-use number_prefix::{NumberPrefix, Prefixed, Standalone};
-
-use crossbeam_channel::bounded;
-
-use std::path::PathBuf;
 use structopt::StructOpt;
+use twox_hash::XxHash;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "xxh", about = "xxHash cli implementation")]
@@ -28,10 +22,6 @@ fn main() {
     let terminal = terminal();
     let cursor = cursor();
 
-    // save the terminal cursor position at the start of the line, so the
-    // progress can be displayed inline
-    cursor.save_position();
-
     for fp in opt.files {
         // open file here to bypass the borrow checker, so we can still use
         // 'fp' to print the filename
@@ -39,6 +29,10 @@ fn main() {
         let filename = fp.display();
         // let metadata = fs::metadata(&fp).unwrap();
         // println!("{}", metadata.len());
+
+        // save the terminal cursor position at the start of the line, so the
+        // progress can be displayed inline
+        cursor.save_position().unwrap();
 
         let (tx_progress, rx_progress) = bounded(1);
         let (tx_result, rx_result) = bounded(1);
@@ -54,7 +48,7 @@ fn main() {
             loop {
                 let consumed = {
                     let bytes = buffer.fill_buf().unwrap();
-                    if bytes.len() == 0 {
+                    if bytes.is_empty() {
                         break;
                     }
                     hasher.write(bytes);
@@ -75,39 +69,25 @@ fn main() {
 
         // poll the queue for hashing progress
         while rx_result.is_empty() {
-
             if rx_progress.is_full() {
-                let elapsed = start.elapsed().unwrap();
-                let micros = elapsed.as_micros(); // - 250000u128;
-                let sec = micros as f64 / 1000000f64;
-                // dbg!(elapsed);
-                // dbg!(micros);
-                // dbg!(sec);
-                let b = rx_progress.recv().unwrap();
-                // dbg!(b);
+                // determine elapsed seconds, to microsecond precision
+                let secs_elapsed = start.elapsed().unwrap().as_micros() as f64 / 1_000_000f64;
+                let bytes_recv = rx_progress.recv().unwrap();
+                let bytes_per_sec = bytes_recv as f64 / secs_elapsed;
 
-                let bytes_sec = b as f64 / sec;
-
-                match NumberPrefix::binary(bytes_sec) {
-                    Standalone(bytes) => print!("{} bytes/s  {}", bytes, filename),
-                    Prefixed(prefix, n) => print!("{:.1} {}B/s  {}", n, prefix, filename),
+                match NumberPrefix::binary(bytes_per_sec) {
+                    Standalone(bytes) => print!("{} bytes/s\t{}", bytes, filename),
+                    Prefixed(prefix, n) => print!("{:.1} {}B/s\t{}", n, prefix, filename),
                 }
 
-                // reset terminal cursor to start of line
-                cursor.reset_position();
+                cursor.reset_position().unwrap();
             }
+            thread::sleep(Duration::from_millis(250));
 
-            // sleep for a few milliseconds
-            thread::sleep(Duration::from_millis(100));
-            
-            // clear the current line after the cursor has been reset to the 
-            // start
-            terminal.clear(ClearType::CurrentLine);
+            terminal.clear(ClearType::CurrentLine).unwrap();
         }
 
-        // join the hashing thread
         handle.join().unwrap();
-
         println!("{:x}  {}", rx_result.recv().unwrap(), filename);
     }
 }
